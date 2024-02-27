81263e45c103d6865249541bfd0f22e003b8af13
Steffen Z <sigma-z@sigma-scripts.de>
2024-02-27 10:04:39 +0100

    add an option page and call acf function like acf_add_local_field_group after acf/init has been processed.

--- app/wp-content/plugins/test-plugin/test-plugin.php
+++ app/wp-content/plugins/test-plugin/test-plugin.php
@@ -3,12 +3,30 @@
 * Plugin Name: Test Plugin
 */
 
-$fieldsJson = @file_get_contents(__DIR__ . '/fields.json');
-if (!$fieldsJson || !function_exists('acf_add_local_field_group')) {
-    return;
-}
 
-$fieldGroups = json_decode($fieldsJson, true) ?: [];
-foreach ($fieldGroups as $group) {
-    acf_add_local_field_group($group);
-}
+add_action('acf/init', function(){
+    $fieldsJson = @file_get_contents(__DIR__ . '/fields.json');
+    if (!$fieldsJson || !function_exists('acf_add_local_field_group')) {
+        return;
+    }
+
+    $fieldGroups = json_decode($fieldsJson, true) ?: [];
+    foreach ($fieldGroups as $group) {
+        acf_add_local_field_group($group);
+    }
+
+    $optionsJson = @file_get_contents(__DIR__ . '/options.json');
+    $optionsPages = json_decode($optionsJson, true) ?: [];
+    foreach ($optionsPages as $group) {
+        acf_add_local_field_group($group);
+    }
+
+    add_action('acf/init', function() {
+        acf_add_options_page([
+            'page_title' => 'Option Page',
+            'menu_slug' => 'option-page',
+            'position' => '',
+            'redirect' => false,
+        ]);
+    });
+}, 20);
